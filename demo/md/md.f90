9ea8ae479a29d1c0b708325dbd3bc59166f5a3b9
David Trevelyan <david.trevelyan@gmail.com>
2015-08-18 18:36:47 +0100

    Removed unused variable from demo MD application

--- demo/md/md.f90
+++ demo/md/md.f90
@@ -3,7 +3,7 @@ program main
     use cpl, only : md_realm, CPL_create_comm
     implicit none
 
-    integer :: rank, size, ierror, MD_COMM
+    integer :: rank, ierror, MD_COMM
 
     call MPI_Init(ierror)
     call CPL_create_comm(md_realm, MD_COMM, ierror)
