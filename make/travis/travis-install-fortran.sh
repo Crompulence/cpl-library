471d4d6b74344c0112f908418e0d06f3bfbf7ec5
Ed Smith <edward.smith05@imperial.ac.uk>
2016-02-16 11:13:58 +0000

    Remove which gfortran command to avoid entire build failure

--- make/travis/travis-install-fortran.sh
+++ make/travis/travis-install-fortran.sh
@@ -3,7 +3,7 @@ set -e
 sudo apt-get update -q
 case $1 in
   gfortran) set -x;
-    sudo apt-get install gfortran;;
+    sudo apt-get install gfortran gfortran-5 -y;;
   ifort) set -x;
     echo "Intel is not a supported fortran compiler for tests:" $1; exit 1;;
   *)
