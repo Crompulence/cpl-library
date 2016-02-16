6fc5434aafc843b12100509251ea6afa78ed7d6d
edwardsmith999 <edward.smith05@imperial.ac.uk>
2016-02-16 11:59:49 +0000

    Cleaned up travis test system and limited test to build only

--- make/travis/travis-install-fortran.sh
+++ make/travis/travis-install-fortran.sh
@@ -3,7 +3,13 @@ set -e
 sudo apt-get update -q
 case $1 in
   gfortran) set -x;
-    sudo apt-get install gfortran gfortran-5 -y;;
+    sudo apt-get install gfortran -y;;
+  gfortran-4.8) set -x;
+    sudo apt-get install gfortran-4.8 -y;;
+  gfortran-4.9) set -x;
+    sudo apt-get install gfortran-4.9 -y;;
+  gfortran-5) set -x;
+    sudo apt-get install gfortran-5 -y;;
   ifort) set -x;
     echo "Intel is not a supported fortran compiler for tests:" $1; exit 1;;
   *)
--- make/travis/travis-install-gcc.sh
+++ make/travis/travis-install-gcc.sh
@@ -4,6 +4,8 @@ sudo apt-get install build-essential
 sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
 sudo apt-get update -q
 case $1 in
+  gcc) set -x;
+    sudo apt-get install gcc g++ -y;;
   gcc-4.8) set -x;
     sudo apt-get install gcc-4.8 g++-4.8 -y;;
   gcc-4.9) set -x;
