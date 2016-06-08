ff2fe6be74906275639290ad55d2ec5529edd0cb
edwardsmith999 <edward.smith05@imperial.ac.uk>
2016-06-08 09:21:19 +0100

    Added cpl_init example and automatically included in sphinx autodoc

---
+++ examples/cpl_init/md_init.f90
@@ -0,0 +1,26 @@
+program main_MD
+   use cpl, only : CPL_init
+   use mpi
+   implicit none
+
+   integer :: rank, nprocs, ierr
+   integer :: MD_COMM
+   integer, parameter :: MD_realm=2
+
+   !Initialise MPI
+   call MPI_Init(ierr)
+
+   !Create MD Comm by spliting world
+   call CPL_init(MD_realm, MD_COMM, ierr)
+
+   call MPI_comm_size(MD_COMM, nprocs, ierr)
+   call MPI_comm_rank(MD_COMM, rank, ierr)
+
+   print*, "MD code processor ", rank+1, " of ", nprocs
+
+   !No need for seperate CPL finalise as MPI finalise takes care of this
+   call MPI_finalize(ierr)
+
+end program main_MD
+
+
