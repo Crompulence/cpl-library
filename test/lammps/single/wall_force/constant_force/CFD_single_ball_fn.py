d4b405e198fb8a1101672344f7e719251e1a7ded
edwardsmith999 <edward.smith05@imperial.ac.uk>
2017-08-11 18:03:00 +0100

    Added lammps test cases for single particles

---
+++ test/lammps/single/wall_force/constant_force/CFD_single_ball_fn.py
@@ -0,0 +1,68 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.widgets import Slider
+from mpi4py import MPI
+
+from cplpy import CPL
+
+
+def CFD(xyzL = [1.5E-003, 1.5E-003, 2.50E-003], 
+        g = 9.81, ncxyz=[8,8,8], npxyz=[1,1,1],
+        Nsteps=101):
+
+    #initialise MPI and CPL
+    comm = MPI.COMM_WORLD
+    CPL = CPL()
+    MD_COMM = CPL.init(CPL.CFD_REALM)
+    nprocs_realm = MD_COMM.Get_size()
+
+    ## Parameters of the cpu topology (cartesian grid)
+    npxyz = np.array(npxyz, order='F', dtype=np.int32)
+    NProcs = np.product(npxyz)
+
+    xyzL = np.array(xyz, order='F', dtype=np.float64)
+    xyz_orig = np.array([0.0, 0.0, 0.0], order='F', dtype=np.float64)
+    ncxyz = np.array(ncxyz, order='F', dtype=np.int32)
+    if (nprocs_realm != NProcs):
+        print("Non-coherent number of processes in MD ", nprocs_realm,
+                " no equal to ",  npxyz[0], " X ", npxyz[1], " X ", npxyz[2])
+        MPI.Abort(errorcode=1)
+
+    #Setup coupled simulation
+    cart_comm = MD_COMM.Create_cart([npxyz[0], npxyz[1], npxyz[2]])
+    CPL.setup_cfd(cart_comm, xyzL, xyz_orig, ncxyz)
+
+    #Get constraint region
+    cnst_limits = CPL.get_cnst_limits();
+    cnst_portion = CPL.my_proc_portion(cnst_limits)
+    [cnst_ncxl, cnst_ncyl, cnst_nczl] = CPL.get_no_cells(cnst_portion)
+
+    #Get overlap region
+    olap_limits = CPL.get_olap_limits()
+    BC_limits = np.array([olap_limits[0], olap_limits[1], 
+                          olap_limits[2], olap_limits[3], 
+                          olap_limits[4], olap_limits[5]], dtype=np.int32)
+    BC_portion = CPL.my_proc_portion(BC_limits)
+    [BC_ncxl, BC_ncyl, BC_nczl] = CPL.get_no_cells(BC_portion)
+
+    #Allocate send and recv arrays
+    recv_array = np.zeros((4, BC_ncxl, BC_ncyl, BC_nczl), order='F', dtype=np.float64)
+    send_array = np.zeros((9, cnst_ncxl, cnst_ncyl, cnst_nczl), order='F', dtype=np.float64)
+
+    for time in range(Nsteps):
+
+        # send data to update
+        send_array[2,:,:,:] = -5.9490638385009208e-08*g  # * mi
+        CPL.send(send_array, cnst_portion)
+
+        # recv data and plot
+        recv_array, ierr = CPL.recv(recv_array, BC_portion)
+
+        print(time)
+
+    CPL.finalize()
+    MPI.Finalize()
+
+
+
+
---
+++ test/lammps/single/wall_force/constant_force/test_bouncing.py
@@ -0,0 +1,99 @@
+import pytest
+from cplpy import run_test, prepare_config
+import subprocess as sp
+import os
+import glob
+import numpy as np
+
+class cd:
+    """Context manager for changing the current working directory"""
+    def __init__(self, newPath):
+        self.newPath = os.path.expanduser(newPath)
+
+    def __enter__(self):
+        self.savedPath = os.getcwd()
+        os.chdir(self.newPath)
+
+    def __exit__(self, etype, value, traceback):
+        os.chdir(self.savedPath)
+
+
+def get_subprocess_error(e):
+    print("subprocess ERROR")
+    import json
+    error = json.loads(e[7:])
+    print(error['code'], error['message'])
+
+
+MD_EXEC = "./lmp_cpl"
+CFD_EXEC = "./CFD_single_ball.py"
+TEST_DIR = os.path.dirname(os.path.realpath(__file__))
+
+@pytest.fixture(scope="module")
+def build_case():
+
+    print("Building LAMMPS")
+    #Try to setup code
+    with cd(TEST_DIR):
+        try:
+            build = sp.check_output("./build.sh", shell=True)
+        except sp.CalledProcessError as e:
+            if e.output.startswith('error: {'):
+                get_subprocess_error(e.output)
+            raise
+
+    return build
+
+@pytest.fixture(scope="module")
+def run_case():
+
+    print("Running case")
+    #Try to run code
+    cmd = ('cplexec -m 1 "' + MD_EXEC + ' < single.in" ' + ' -c 1 ' +  CFD_EXEC)
+
+    with cd(TEST_DIR):
+        try:
+            clean = sp.check_output("rm -f ./thermo_output* ./log.lammps* ./debug.vels", 
+                                    shell=True)
+            run = sp.check_output(cmd, shell=True)
+        except sp.CalledProcessError as e:
+            if e.output.startswith('error: {'):
+                get_subprocess_error(e.output)
+            raise
+
+    return run
+
+
+@pytest.fixture(scope="module")
+def build_run():
+    build = build_case()
+    run = run_case()
+
+def test_gravity(build_run):
+
+    #Check vs analystical solution for gravity
+    import bouncing
+
+    with cd(TEST_DIR):
+        error = bouncing.check_bouncing_error_vs_gravity()
+        for e in error[0,1,:]:
+            assert np.abs(e) < 1e-11
+
+
+def test_regression(build_run):
+
+    #Check vs analystical solution for gravity
+    import bouncing as b
+    with cd(TEST_DIR):
+
+        t, z, v, f = b.read_data(logfile='./log.lammps', 
+                                 datafile='./thermo_output.txt')
+
+        t_reg, z_reg, v_reg, f_reg = b.read_data(logfile='./regression_data/log.lammps', 
+                                     datafile='./regression_data/thermo_output.txt')
+
+        for i in range(z.shape[0]):
+            assert np.abs(z[i]-z_reg[i]) < 1e-12
+            assert np.abs(v[i]-v_reg[i]) < 1e-12
+            assert np.abs(f[i]-f_reg[i]) < 1e-12
+
