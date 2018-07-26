162702bb8683c5937d526dafeb5b7c5c23cf1b5c
edwardsmith999 <edward.smith05@imperial.ac.uk>
2018-07-26 09:46:09 +0100

    Changed minimal MD python script to use one process

--- examples/minimal_send_recv_mocks/minimal_MD.py
+++ examples/minimal_send_recv_mocks/minimal_MD.py
@@ -5,7 +5,7 @@ from cplpy import CPL
 comm = MPI.COMM_WORLD
 CPL = CPL()
 MD_COMM = CPL.init(CPL.MD_REALM)
-CPL.setup_md(MD_COMM.Create_cart([2, 2, 2]), xyzL=[1.0, 1.0, 1.0], 
+CPL.setup_md(MD_COMM.Create_cart([1, 1, 1]), xyzL=[1.0, 1.0, 1.0], 
              xyz_orig=[0.0, 0.0, 0.0])
 recv_array, send_array = CPL.get_arrays(recv_size=1, send_size=4)
 
