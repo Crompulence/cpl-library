439f2997fc2131fa76e4d90900736fffbce7e00a
Ed <es205@imperial.ac.uk>
2019-04-08 02:49:10 +0100

    Added brackets to print in Python sendrecv example

--- examples/sendrecv_globcell/python/cfd_send_cells.py
+++ examples/sendrecv_globcell/python/cfd_send_cells.py
@@ -24,7 +24,7 @@ CFD_COMM = CPL.init(CPL.CFD_REALM)
 nprocs_realm = CFD_COMM.Get_size()
 
 if (nprocs_realm != NProcs):
-    print "ERROR: Non-coherent number of processors."
+    print("ERROR: Non-coherent number of processors.")
     MPI.Abort(errorcode=1)
 
 cart_comm = CFD_COMM.Create_cart([NPx, NPy, NPz])
