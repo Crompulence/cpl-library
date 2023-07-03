543787f97e79af1eb9f9a27391372152797a7fa2
Gavin Pringle <gavin@epcc.ed.ac.uk>
2023-07-03 18:10:04 +0100

    Update example_archer2.bat
    Now uses modules by default, but provides lines on how to use your own installation.

--- examples/LAMMPS_OPENFOAM/example_archer2.bat
+++ examples/LAMMPS_OPENFOAM/example_archer2.bat
@@ -11,15 +11,21 @@
 # single thread export overriders any declaration in srun
 export OMP_NUM_THREADS=1
 
-module load openfoam/com/v2106
-module switch gcc gcc/10.3.0
-module load cray-fftw
-module load cray-python
-module load matplotlib
+module load other-software
+module load cpl-openfoam
+source $FOAM_CPL_APP/SOURCEME.sh
+module load cpl-lammps
 
-source /work/y23/shared/cpl-openfoam-lammps/cpl-library/SOURCEME.sh
-source /work/y23/shared/cpl-openfoam-lammps/CPL_APP_OPENFOAM/SOURCEME.sh
-source /work/y23/shared/cpl-openfoam-lammps/CPL_APP_LAMMPS-DEV/SOURCEME.sh
+# using your own installtion: remove the previous three lines and use these four 'module' lines and three 'source' lines instead
+# remmeber to update the path to the three SOURCEME.sh files
+#module load openfoam/com/v2106
+#module switch gcc gcc/10.3.0
+#module load cray-fftw
+#module load cray-python
+#module load matplotlib
+#source /work/y23/shared/cpl-openfoam-lammps/cpl-library/SOURCEME.sh
+#source /work/y23/shared/cpl-openfoam-lammps/CPL_APP_OPENFOAM/SOURCEME.sh
+#source /work/y23/shared/cpl-openfoam-lammps/CPL_APP_LAMMPS-DEV/SOURCEME.sh
 
 cd /work/y23/shared/cpl-openfoam-lammps/cpl-library/examples/LAMMPS_OPENFOAM/openfoam
 python clean.py -f
