6d696064a97fee4e2f56981bc0fcf6f8ebde2f3d
Ed <edward.smith0@brunel.ac.uk>
2023-12-07 17:02:41 +0000

    Added updated dummy code for CFD and MD

--- examples/LAMMPS_OPENFOAM/plot_coupled.py
+++ examples/LAMMPS_OPENFOAM/plot_coupled.py
@@ -8,6 +8,12 @@ ppdir = '/home/es205/codes/python/pyDataView/'
 sys.path.append(ppdir)
 import postproclib as ppl
 
+# Font size and type
+fsize = 24
+plt.rc('font', size=fsize)
+plt.rc('font', **{'family': 'serif', 'serif': ['Computer Modern']})
+plt.rc('text', usetex=True)
+
 #Get Post Proc Object
 fdir = './'
 PPObj = ppl.CPL_PostProc(fdir)
@@ -25,20 +31,20 @@ xyzL = [X.max(), Y.max(), Z.max()]
 nxyz = [X.shape[0], Y.shape[0], Z.shape[0]]
 U = 1.0
 nu = CFDObj.Raw.nu[0]
-Re = (xyzL[1]+dy)/nu
+Re = (xyzL[1]-dy)/nu
 rec_dt = float(CFDObj.Raw.header.headerDict['controlDict']['writeInterval'])
  
 fig, ax = plt.subplots(1,1)
 #plt.ion()
 #plt.show()
 
-ave = 25
-for rec in [25,50,100,250,750]:#plotObj.maxrec-1-ave]:
+ave = 0
+for rec in [25,50,100,250,790]:#plotObj.maxrec-1-ave]:
 
     #Get coupled solutions
     yMD, uMD, yCFD, uCFD = plotObj.profile_both(axis=1, startrec=rec-ave, endrec=rec+ave)
 
-    ymin = yMD[3]
+    ymin = yMD[0]
 #    ax.plot(uMD[:-12,0], yMD[:-12]-4.33021, 'ro')
 #    ax.plot(uCFD[:,0], yCFD-9.296, 'bx')
 
@@ -57,13 +63,18 @@ for rec in [25,50,100,250,750]:#plotObj.maxrec-1-ave]:
     ax.plot(np.mean(haloCPL[...,0],(0,2,3)), yCFD[0 ]-dy/2., 'gs')
     ax.plot(np.mean(haloCFD[...,0],(0,2,3)), yCFD[-1]+dy/2., 'gs')
 
+    ax.plot(uMD[6,0], yMD[6], 'rs')
+
     #Get analytical solns
-    CAObj = CA(Re=Re, U=U, Lmin=ymin, Lmax=yCFD[-1]+dy/2., npoints=2*nxyz[1]+2)
+    CAObj = CA(Re=Re, U=U, Lmin=ymin+2., Lmax=yCFD[-1]+dy/2., npoints=2*nxyz[1]+2)
     y_anal, u_anal = CAObj.get_vprofile(rec*rec_dt)
     ax.plot(u_anal, y_anal, 'k-')
 
-
+    
     #plt.pause(0.001)
     #plt.cla()
-
+plt.ylabel("$y$")
+plt.xlabel("$u$")
 plt.show()
+#plt.savefig("Coupled_Couette.eps", bbox_inches="tight")
+#plt.savefig("Coupled_Couette.pdf", bbox_inches="tight")
